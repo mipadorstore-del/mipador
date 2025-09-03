38afecc2bcbe3bea7c2edb5be1e956dda9834e57
anas <anasanasri3030@gmail.com>
2025-09-03 13:51:42 +0100

    new design sheets

--- js/proxy.js
+++ js/proxy.js
@@ -1,18 +1,18 @@
-export async function handler(event, context) {
-  const response = await fetch("https://script.google.com/macros/s/AKfycbzIipteyDYaUeVA1qpamd9J9hCA4KOPkOq-jzyMn6VSmOizU9A50JwXDSEYVJk_bxQ_rg/exec", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: event.body
-  });
+// export async function handler(event, context) {
+//   const response = await fetch("https://script.google.com/macros/s/AKfycbzIipteyDYaUeVA1qpamd9J9hCA4KOPkOq-jzyMn6VSmOizU9A50JwXDSEYVJk_bxQ_rg/exec", {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: event.body
+//   });
 
-  const data = await response.text();
+//   const data = await response.text();
 
-  return {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Content-Type": "application/json"
-    },
-    body: data
-  };
-}
+//   return {
+//     statusCode: 200,
+//     headers: {
+//       "Access-Control-Allow-Origin": "*",
+//       "Content-Type": "application/json"
+//     },
+//     body: data
+//   };
+// }
